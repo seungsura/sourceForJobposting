51dc93f2408360afaa976f514c48846ab24b43fa
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-20 16:38:54 +0900

    Update Makemodelview.sh

--- Makemodelview.sh
+++ Makemodelview.sh
@@ -4,5 +4,5 @@ cat source/backend.web.config.urls.py > backend/web/config/urls.py
 cat source/backend.web.todo.urls.py > backend/web/todo/urls.py
 cat source/backend.web.todo.serializers.py > backend/web/todo/serializers.py
 cat source/backend.web.todo.views.py > backend/web/todo/views.py
-docker-compose run --rm web sh -c "python manage.py collectstatic"
+docker-compose run --rm web sh -c "python manage.py collectstatic" --no-input
 docker-compose up -d --build
