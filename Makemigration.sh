c6f57f433229669679f0dc0fe521e4889584e328
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-20 11:02:00 +0900

    Update Makemigration.sh

--- Makemigration.sh
+++ Makemigration.sh
@@ -11,3 +11,5 @@ cat source/todo.admin.py > backend/web/todo/admin.py
 docker-compose run --rm web sh -c "python manage.py makemigrations"
 sleep 5s
 docker-compose run --rm web sh -c "python manage.py migrate"
+sleep 5s
+docker-compose run --rm web sh -c "python create_superuser.py"
