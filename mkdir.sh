c08eb39381b66bb863c156806fdced5c755cb42d
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-20 11:00:37 +0900

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -19,7 +19,7 @@ mkdir frontend/nginx
 mkdir frontend/web-front
 touch backend/web/.env
 touch backend/web/Dockerfile
-touch backend/web/create_superuser    #add to
+touch backend/web/create_superuser.py    #add to
 touch backend/web/requirements.txt
 touch backend/nginx/Dockerfile.dev
 touch backend/nginx/default.dev.conf
