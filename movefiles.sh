6afd8c3dae722c462509332a5f0c914223fb7373
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-20 10:58:24 +0900

    add file
    create_superuser

--- movefiles.sh
+++ movefiles.sh
@@ -3,11 +3,11 @@
 cat source/backend.web.env > backend/web/.env
 cat source/backend.web.Dockerfile > backend/web/Dockerfile
 cat source/backend.web.requirements.txt > backend/web/requirements.txt
+cat source/backend.web.create_superuser > backend/web/create_superuser  #add to
 cat source/backend.nginx.Dockerfile > backend/nginx/Dockerfile.dev
 cat source/backend.nginx.default.conf > backend/nginx/default.dev.conf
 cat source/mariadb.master.env > mariadb/master/.env  #changed
 cat source/mariadb.slave.env > mariadb/slave/.env    #changed
-#cat source/mariadb.Dockerfile > mariadb/Dockerfile   #remove
 cat source/mariadb.master.my.cnf > mariadb/master/config/my.cnf           #changed
 cat source/mariadb.slave.my.cnf > mariadb/slave/config/my.cnf           #changed
 cat source/mariadb.master.sql.init.sql > mariadb/master/sql/init.sql    #changed
