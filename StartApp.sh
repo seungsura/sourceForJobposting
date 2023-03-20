7c582f1e3393b569c366530bc96cb044520a5866
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-20 11:05:49 +0900

    Update StartApp.sh

--- StartApp.sh
+++ StartApp.sh
@@ -5,4 +5,4 @@ docker-compose run --rm web sh -c "python manage.py startapp todo"
 touch backend/web/config/local_settings.py
 cat source/local_settings.py > backend/web/config/local_settings.py
 
-docker-compose up --build
+docker-compose up -d --build
