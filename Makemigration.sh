f1c1b1cdb0649d514c6159668aabfe10f2bbff51
seungsura <90497686+seungsura@users.noreply.github.com>
2023-03-23 08:23:05 +0900

    Update Makemigration.sh

--- Makemigration.sh
+++ Makemigration.sh
@@ -3,7 +3,7 @@
 DjangoSettingsFile=./backend/web/config/
 
 Secret_Key=`grep SECRET_KEY ${DjangoSettingsFile}/settings.py`
-sed -i "s@SECRET_KEY@${Secret_Key}@g" source/backend.web.config.settings.py
+sed -i "s/SECRET_KEY/${Secret_Key}/g" source/backend.web.config.settings.py
 cat source/backend.web.config.settings.py > backend/web/config/settings.py
 cat source/todo.models.py > backend/web/todo/models.py
 cat source/todo.admin.py > backend/web/todo/admin.py
